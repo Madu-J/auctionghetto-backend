c54ac4b50d8f47ff99f8657effbafd808446262c
Madu-J Omenka <madu.omenka@hotmail.com>
2024-06-17 11:54:43 +0000

    Updated environmental vitualenv and file

--- auctionghetto_api/settings.py
+++ auctionghetto_api/settings.py
@@ -67,7 +67,7 @@ ALLOWED_HOSTS = [
     'localhost', 
     'auctionghetto-api-17774afbeb21.herokuapp.com',
     '8000-maduj-auctionghettobac-3c7m7qoq92q.ws-eu111.gitpod.io',
-    '8000-maduj-auctionghettobac-3c7m7qoq92q.ws-eu113.gitpod.io'
+    '8000-maduj-auctionghettobac-ern4y3112wa.ws-eu114.gitpod.io'
 ]
 
 
