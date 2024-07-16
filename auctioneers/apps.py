316c6194b25b3a3f0e0bfc5705da4e819ce187b5
Madu Omenka <madu.omenka@hotmail.com>
2024-07-16 09:49:53 +0000

    Fixed error in app config

--- auctioneers/apps.py
+++ auctioneers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuctioneerConfig(AppConfig):
+class AuctioneersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auctioneers'
